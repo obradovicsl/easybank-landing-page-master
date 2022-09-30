2fe6e3162e689b7cc00e99fd2d92265aa3f548bc
obradovicsl <57071443+obradovicsl@users.noreply.github.com>
2022-09-30 20:56:30 +0200

    Touch ups
    Increased mobile breakpoint from 375 to 400px
    Disabled scrolling, when mobile navigation is showing

--- script.js
+++ script.js
@@ -2,14 +2,30 @@ const hamburgerEl = document.querySelector('.hamburger-menu');
 const backdropEl = document.querySelector('.backdrop');
 const mobileNavEl = document.querySelector('.mobile-nav');
 
+const blockScroll = function () {
+    this.window.scrollTo(0, 0);
+}
+
 hamburgerEl.addEventListener('click', function () {
     hamburgerEl.classList.toggle('active');
     backdropEl.classList.toggle('hidden');
     mobileNavEl.classList.toggle('hidden');
+
+    if (hamburgerEl.classList.contains('active')) {
+
+        //first detect scroll
+        window.addEventListener('scroll', blockScroll)
+        //And when user scroll, move it back on top of the page
+    }
+    else {
+        alert();
+        window.removeEventListener('scroll', blockScroll);
+    }
 });
 
 backdropEl.addEventListener('click', function () {
     hamburgerEl.classList.toggle('active');
     backdropEl.classList.toggle('hidden');
     mobileNavEl.classList.toggle('hidden');
+    window.removeEventListener('scroll', blockScroll);
 })
